18a87e776079f7cbb287783a1b96f8621fd5070c
Gouse Baig <gbaig@wisys.com.sa>
2025-09-10 18:18:32 +0300

    Added Web and API projects

---
+++ FullStackAuth.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FullStackAuth.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
